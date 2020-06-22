69dec0c9b632fef745dc11ee6607412a1ffad397
Aquiles Carattino <aqui.carattino@gmail.com>
2020-06-22 07:49:37 +0200

    Added better support for static files

--- aqui_brain_dump/main.py
+++ aqui_brain_dump/main.py
@@ -1,3 +1,4 @@
+from distutils.dir_util import copy_tree
 from jinja2 import Template
 import markdown
 import os
@@ -5,22 +6,31 @@ import os
 
 dir = os.path.abspath(os.path.join('..', 'content'))
 out_dir = os.path.abspath(os.path.join('..', 'output'))
+static_dir = os.path.abspath(os.path.join('..', 'static'))
 template_dir = os.path.abspath(os.path.join('..', 'templates'))
 
 if not os.path.isdir(out_dir):
     os.makedirs(out_dir)
 
+out_static_dir = os.path.join(out_dir, 'static')
+if not os.path.isdir(out_static_dir):
+    os.makedirs(out_static_dir)
+copy_tree(static_dir, out_static_dir)
+
 with open(os.path.join(template_dir, 'base.html'), 'r') as f:
     template = Template(f.read())
+
 
 f_walk = os.walk(dir)
 for dirs in f_walk:
     cur_dir = dirs[0]
-    print(cur_dir)
     for file in dirs[2]:
-        print(file)
         with open(os.path.join(cur_dir, file), 'r') as f:
             filename = ''.join(file.split('.')[:-1])
             html = markdown.markdown(f.read(), extensions=['wikilinks', ])
             with open(os.path.join(out_dir, filename+'.html'), 'w') as out_file:
-                out_file.write(template.render(content=html))
+                context = {
+                    'content': html,
+                    'static': 'static',
+                }
+                out_file.write(template.render(context))
