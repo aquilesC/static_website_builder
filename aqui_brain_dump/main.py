7a505223d52e58b5c1021f88987a0dda2e0c045c
Aquiles Carattino <aqui.carattino@gmail.com>
2020-06-23 23:12:04 +0200

    Added markdown extensions

--- aqui_brain_dump/main.py
+++ aqui_brain_dump/main.py
@@ -3,7 +3,6 @@ from jinja2 import Template
 import markdown
 import os
 
-
 dir = os.path.abspath(os.path.join('..', 'content'))
 out_dir = os.path.abspath(os.path.join('..', 'output'))
 static_dir = os.path.abspath(os.path.join('..', 'static'))
@@ -20,7 +19,6 @@ copy_tree(static_dir, out_static_dir)
 with open(os.path.join(template_dir, 'base.html'), 'r') as f:
     template = Template(f.read())
 
-
 f_walk = os.walk(dir)
 for dirs in f_walk:
     cur_dir = dirs[0]
@@ -29,7 +27,17 @@ for dirs in f_walk:
             continue
         with open(os.path.join(cur_dir, file), 'r') as f:
             filename = ''.join(file.split('.')[:-1])
-            html = markdown.markdown(f.read(), extensions=['wikilinks', 'mdx_gh_links'])
+            html = markdown.markdown(f.read(),
+                                     extensions=[
+                                         'wikilinks',
+                                         'admonition',
+                                         'meta',
+                                         'markdown_checklist.extension',
+                                         'fenced_code',
+                                         'codehilite',
+                                         'pyembed.markdown',
+                                     ])
+
             dest_folder = os.path.relpath(os.path.join(cur_dir, file), start=dir)
             os.makedirs(os.path.join(out_dir, dest_folder, filename), exist_ok=True)
             with open(os.path.join(out_dir, dest_folder, filename, 'index.html'), 'w') as out_file:
