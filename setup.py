2c926e2d2e57aa69735ea96c64aeed6a254a9623
Aquiles Carattino <aqui.carattino@gmail.com>
2020-07-04 18:19:17 +0200

    Added postcss to compress the output file

---
+++ setup.py
@@ -0,0 +1,19 @@
+#  Copyright (c) 2020. Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+#  Morbi non lorem porttitor neque feugiat blandit. Ut vitae ipsum eget quam lacinia accumsan.
+#  Etiam sed turpis ac ipsum condimentum fringilla. Maecenas magna.
+#  Proin dapibus sapien vel ante. Aliquam erat volutpat. Pellentesque sagittis ligula eget metus.
+#  Vestibulum commodo. Ut rhoncus gravida arcu.
+
+from setuptools import setup
+
+setup(
+    name='aqui_brain_dump',
+    version='1.0',
+    packages=[''],
+    package_dir={'': 'static'},
+    url='https://github.com/aquilesC/static_website_builder',
+    license='BSDv3',
+    author='Aquiles Carattino',
+    author_email='aqui.carattino@gmail.com',
+    description='Convert a bunch of markdown files to a beautiful website'
+)
--- templates/postcss.config.js
+++ templates/postcss.config.js
@@ -2,11 +2,11 @@ module.exports = {
     plugins: [
         require('tailwindcss'),
         require('autoprefixer'),
-        // require('@fullhuman/postcss-purgecss')({
-        //     content: [
-        //         './*/*.html',
-        //     ],
-        //     defaultExtractor: content => content.match(/[A-Za-z0-9-_:/]+/g) || []
-        // })
+        require('@fullhuman/postcss-purgecss')({
+            content: [
+                './*.html',
+            ],
+            defaultExtractor: content => content.match(/[A-Za-z0-9-_:/]+/g) || []
+        })
     ]
 }
