aa68fe095a020a9d1dec6466260c6115d6c99f6e
Aquiles Carattino <aqui.carattino@gmail.com>
2020-07-22 12:45:26 +0200

    Improved link building by changing links to absolute

--- aqui_brain_dump/main.py
+++ aqui_brain_dump/main.py
@@ -11,8 +11,9 @@ from aqui_brain_dump.backlinks_wikilinks import WikiLinkExtension
 
 THIS_DIR = os.getcwd()
 
+
 def main(
-        base_website="https://www.aquiles.me/",
+        base_website="https://www.aquiles.me",
         content_dir='content',
         output_dir='output',
         static_dir='static',
@@ -25,7 +26,7 @@ def main(
     dir = os.path.abspath(os.path.join(THIS_DIR, content_dir))
     out_dir = os.path.abspath(os.path.join(THIS_DIR, output_dir))
     static_dir = os.path.abspath(os.path.join(THIS_DIR, static_dir))
-    template_dir = os.path.abspath(os.path.join(THIS_DIR,template_dir))
+    template_dir = os.path.abspath(os.path.join(THIS_DIR, template_dir))
 
     if not os.path.isdir(out_dir):
         os.makedirs(out_dir)
@@ -34,7 +35,6 @@ def main(
     if not os.path.isdir(out_static_dir):
         os.makedirs(out_static_dir)
     copy_tree(static_dir, out_static_dir)
-
 
     md = markdown.Markdown(extensions=[
         'meta',
@@ -52,10 +52,15 @@ def main(
     for dirs in f_walk:
         cur_dir = dirs[0]
         sub_dir = os.path.relpath(cur_dir, start=dir)
-        if sub_dir == '.': sub_dir = ''
+        if sub_dir == '.':
+            sub_dir = ''
+
         if sub_dir.startswith('.'):
             continue
-        if sub_dir: sub_dir = f'/{sub_dir}'
+
+        if sub_dir:
+            sub_dir = f'/{sub_dir}'
+
         for file in dirs[2]:
             if not file.endswith('.md'):
                 copyfile(os.path.join(cur_dir, file), os.path.join(out_dir, sub_dir, file))
@@ -64,7 +69,7 @@ def main(
             filename = ''.join(file.split('.')[:-1])
             page_url = f"{sub_dir}/{filename}".lower()
             page_url = page_url.replace(' ', '_')
-            if not page_url in pages:
+            if page_url not in pages:
                 pages[page_url] = dict(content=None, links=[], meta={}, filename='', url='')
 
             with codecs.open(os.path.join(cur_dir, file), 'r', encoding='utf-8') as f:
@@ -80,13 +85,13 @@ def main(
                 })
                 for link in md.links:
                     link = link.replace(' ', '_').lower()
+                    link = f"/{link}"
                     if link not in pages:
                         pages[link] = dict(content=None, links=[], meta={}, filename=link, url=base_website+link)
 
                     pages[link]['links'].append(
                         page_url if not file == index_page else f'{sub_dir}/'
                     )
-                    print(f'{page_url}')
 
     env = Environment(loader=FileSystemLoader(template_dir))
     template_article = env.get_template('article.html')
@@ -94,8 +99,8 @@ def main(
 
     for page, values in pages.items():
         if page.startswith('/'): page = page[1:]
-        print(f"Creating {page}")
         os.makedirs(os.path.join(out_dir, page), exist_ok=True)
+        print(f'Creating {page}')
         context = {
             'title': values['filename'].replace('_', ' '),
             'content': values['content'],
@@ -105,6 +110,7 @@ def main(
             'url': values['url'],
         }
         context.update(values['meta'])
+
         if index_page.startswith(page):
             with open(os.path.join(out_dir, 'index.html'), 'w') as out_file:
                 out_file.write(template_index.render(context))
