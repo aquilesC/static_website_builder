81a53e23830b3141adf4e5aaf970d4bb012eb8ea
Aquiles Carattino <aqui.carattino@gmail.com>
2020-09-28 20:08:58 +0200

    Styling improvements, removed color from links and is now a box shadow of colors. Change width of main text on large screen. Increase font on sm devices

--- templates/tailwind.config.js
+++ templates/tailwind.config.js
@@ -11,7 +11,16 @@ module.exports = {
                 serif: ['Playfair Display'],
                 sans: ['Poppins'],
                 head: ['Red Hat Text, sans-serif'],
+                brand: ['Raleway'],
             }
+        },
+        boxShadow: {
+            sm: '0 1px 2px 0 rgba(0, 0, 0, 0.05)',
+            lg: '0 10px 15px -3px rgba(0, 0, 0, 0.1), 0 4px 6px -2px rgba(0, 0, 0, 0.05)',
+            default: '0 1px 3px 0 rgba(0, 0, 0, 0.1), 0 1px 2px 0 rgba(0, 0, 0, 0.06)',
+            link: 'inset 0 -4px 0 0 rgba(178, 245, 234, .7)',
+            wikilink: 'inset 0 -4px 0 0 rgba(251, 211, 141, .7)',
+            none: 'none',
         },
     },
     variants: {},
