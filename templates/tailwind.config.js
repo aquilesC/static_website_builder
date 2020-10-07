f9bbb3403e10cdff7fb947fb3dea6447d8601917
Aquiles Carattino <aqui.carattino@gmail.com>
2020-10-07 17:07:18 +0200

    Updated templates, removed sidebar for backlinks, simplified font selection

--- templates/tailwind.config.js
+++ templates/tailwind.config.js
@@ -9,15 +9,16 @@ module.exports = {
         extend: {
             fontFamily: {
                 serif: ['Playfair Display'],
-                sans: ['Poppins'],
-                head: ['Red Hat Text, sans-serif'],
+                sans: ['geomanistregular'],
+                head: ['geomanistbold'],
+                strong: ['geomanistbook'],
                 brand: ['Raleway'],
             },
             boxShadow: {
                 link: '0 -4px 0 0 rgba(178, 245, 234, .7) inset',
                 linkhover: '0 -6px 0 0 rgba(178, 245, 234, .7) inset',
-                wikilink: '0 -4px 0 0 rgba(251, 211, 141, .7) inset',
-                wikihover: '0 -6px 0 0 rgba(251, 211, 141, .7) inset',
+                wikilink: '0 -4px 0 0 rgba(255, 208, 0, .9) inset',
+                wikihover: '0 -12px 0 0 rgba(255, 208, 0, .6) inset',
             },
             backgroundImage: theme => ({
                 'hero-image': "url('/static/Aquiles.jpg')",
